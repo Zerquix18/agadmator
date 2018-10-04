f18c8168bca9b4de25304003420ca0eaf5917fa1
I'm Luis! ^-^ <malbertoa_11@hotmail.com>
2018-10-04 17:08:05 -0400

    Moved video items to new component

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -77,35 +77,18 @@
   </div>
   <div v-else>
     <div class="columns is-multiline">
-      <div v-for="video in result" class="column is-one-third">
-        <div class="card">
-          <div class="card-image">
-            <figure class="video-container">
-              <embed :src="`https://www.youtube.com/embed/${video.id}`" alt="Placeholder image">
-            </figure>
-          </div>
-          <div class="card-content">
-            <div class="media">
-              <div class="media-content">
-                <p class="title is-4">{{ video.white_pieces }} vs {{ video.black_pieces }}</p>
-                <p v-if="video.game_name" class="subtitle is-6">{{ video.game_name }}</p>
-                <p class="subtitle is-6">{{ video.event }}</p>
-                <p class="subtitle is-6">{{ video.opening }}</p>
-              </div>
-            </div>
-            <div class="content">
-              <time>Uploaded on {{ video.video_date }}</time>
-            </div>
-          </div>
-        </div>
-      </div>
+      <VideoItem v-for="video in result" v-bind:key="video.id" :video="video" />
     </div>
   </div>
 </div>
 </template>
 
 <script>
+
+import VideoItem from './VideoItem.vue';
+
 export default {
+  components: {VideoItem},
   data: function () {
     return {
       autocomplete: {
---
+++ resources/js/components/VideoItem.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="column is-one-third">
+  <div class="card">
+    <div class="card-image">
+      <figure class="video-container">
+        <embed :src="`https://www.youtube.com/embed/${video.id}`" alt="Placeholder image">
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-content">
+          <p class="title is-4">{{ video.white_pieces }} vs {{ video.black_pieces }}</p>
+          <p v-if="video.game_name" class="subtitle is-6">{{ video.game_name }}</p>
+          <p class="subtitle is-6">{{ video.event }}</p>
+          <p class="subtitle is-6">{{ video.opening }}</p>
+        </div>
+      </div>
+      <div class="content">
+        <time>Uploaded on {{ video.video_date }}</time>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['video'],
+}
+</script>
